bac7f3e7f65d114cf7246ecb270efae711fff6dd
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-28 12:42:56 +0100

    Add files via upload

---
+++ ocjenskaVjezbaString03/ocjenskaVjezbaString03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocjenskaVjezbaString03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu:");
+            string unos = Console.ReadLine();
+
+            string[] rečenica = unos.Split(' ');
+            string prvariječ = rečenica[0];
+            string zadnjariječ = rečenica[rečenica.Length - 1];
+
+            Console.WriteLine("Prva riječ je:" + prvariječ);
+            Console.WriteLine("Zadnja riječ je:" + zadnjariječ);
+
+            Console.ReadKey();
+        }
+    }
+}
